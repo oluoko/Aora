71630e0122edd0b8ed45b8400c631caf700bdf02
oluoko <brianotieno586@gmail.com>
2024-12-17 16:00:55 +0300

    First Commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import "./global.css";
+
+const index = () => {
+  return (
+    <View className="bg-slate-900/40 text-white">
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
