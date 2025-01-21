45af198f8c1297a287974eca2a491f925c32a322
oluoko <brianotieno586@gmail.com>
2025-01-22 00:13:31 +0300

    Aora: Updated Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-const Index = () => {
+export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-slate-900">
       <Text className="text-white text-2xl">Hello NativeWind!</Text>
     </View>
   );
-};
-
-export default Index;
+}
