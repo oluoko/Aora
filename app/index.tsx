8a63c0109b38228a9ef4a115f22e61e70c5e54ae
oluoko <brianotieno586@gmail.com>
2025-01-22 12:23:14 +0300

    Aora

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-slate-900">
-      <Text className="text-white text-2xl">Hello NativeWind!</Text>
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" className="text-blue-400">
+        Go To Profile
+      </Link>
     </View>
   );
 }
